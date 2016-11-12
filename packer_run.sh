85b5707790509632a538e2a9a09a8baae236d4a0
MeenachiSundaram <meenachisundaram.v@gmail.com>
2016-11-12 15:34:16 +0530

    location issue updates

--- packer_run.sh
+++ packer_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /packer/centos-7.2/jenkins_packer && packer build -force -var-file=centos72.json centos.json
+cd /packer/centos-7.2/jenkins_packer_ansible_build && packer build -force -var-file=centos72.json centos.json
