9b7a0a0ddefd5d4d10b2f0c9fb1a0adc04280d90
MeenachiSundaram <meenachisundaram.v@gmail.com>
2016-11-12 13:57:29 +0530

    forcing packer run

--- packer_run.sh
+++ packer_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /packer/centos-7.2/jenkins_packer && packer build -var-file=centos72.json centos.json
+cd /packer/centos-7.2/jenkins_packer && packer build -force -var-file=centos72.json centos.json
