14e32e79fb6caf64dc476b3a869564e4f48c399a
MeenachiSundaram <meenachisundaram.v@gmail.com>
2016-11-12 12:24:29 +0530

    ansible & ansible container installation

--- script/custom-script.sh
+++ script/custom-script.sh
@@ -21,3 +21,17 @@ rpm -Uvh *.rpm
 echo "Copying 'docker-compose' 'docker-machine' & 'kubectl' and adding to PATH"
 #Copying 'docker-compose' 'docker-machine' & 'kubectl'
 cp /opt/local_files/bin_files/* /usr/bin/.
+
+echo "Installing Ansible & Ansible Container"
+#Installing Ansible & Ansible Container
+sudo yum install ansible
+sudo yum install gcc
+cd /usr/src
+wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
+tar xzf Python-2.7.12.tgz
+cd Python-2.7.12
+./configure
+make altinstall
+sudo python get-pip.py
+wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+sudo pip install ansible-container
